19e93c3d598ce2430ff4d68d5b5d375d2743634b
Sebastian Schulz <sebastian.schulz95@gmail.com>
2016-08-07 15:52:11 +0200

    added output and offset correction for nel worker

--- python-worker/namespaces.py
+++ python-worker/namespaces.py
@@ -7,3 +7,4 @@ from rdflib import Namespace
 mcas = Namespace('http://s16a/vocab/mcas/1.0/')
 oa = Namespace('http://www.w3.org/ns/oa#')
 nif = Namespace('http://persistence.uni-leipzig.org/nlp2rdf/ontologies/nif-core#')
+itsrdf = Namespace('http://www.w3.org/2005/11/its/rdf#')
--- python-worker/pdf_text_named_entity_linking_worker.py
+++ python-worker/pdf_text_named_entity_linking_worker.py
@@ -6,14 +6,15 @@ import operator
 from os import path
 from worker_util import create_annotation, get_cache_filename
 from RAKE import rake
-from rdflib import URIRef, Literal, Graph
-from rdflib.namespace import XSD, RDF
+from rdflib import URIRef, Literal, Graph, BNode
+from rdflib.namespace import XSD, RDF, DC
 import requests
 from json import loads
 import re
 
 
 kea_url = 'http://141.89.225.50/kea-2.0.1/services/annotate'
+max_length = 250
 
 class PdfTextNamedEntityLinkingWorker(AbstractWorker):
 
@@ -28,42 +29,12 @@ class PdfTextNamedEntityLinkingWorker(AbstractWorker):
         model_filename = get_cache_filename(url)
         json_filename = model_filename + '.data.txt.json'
 
-        # with open(json_filename, 'r') as json_file:
-            # text_dict = loads(json_file.read())
-
-        # for paragraph in text_dict:
-            # title = paragraph['title']
-            # content = paragraph['content']
-
-            # for sentence in content:
-
-                # nif = Graph()
-                # data_uri = URIRef(url)
-                # print(url)
-                # print(data_uri)
-
-                # ref = URIRef(url + '#char=0,' + str(len(sentence)))
-                # print(ref)
-
-
-                # nif.add((ref, RDF.type, namespaces.nif.RFC5147String))
-                # nif.add((ref, RDF.type, namespaces.nif.String))
-                # nif.add((ref, RDF.type, namespaces.nif.Context))
-
-                # nif.add((ref, namespaces.nif.isString, Literal(sentence, datatype=XSD.String)))
-
-                # nif_text = nif.serialize(format='turtle')
-
-                # print('lorem')
-                # response = requests.post(kea_url, data=nif_text)
-
-                # print(response.content.decode('utf-8'))
-                # print('ipsum')
-
         with open(text_filename, encoding='utf-8') as file:
             text = file.read()
 
         current_char_count = 0
+        
+        annotations = self.get_new_model()
 
         for sentence in re.split(r'((\.|\?|\!)(\s))', text):
             nif = Graph()
@@ -71,28 +42,58 @@ class PdfTextNamedEntityLinkingWorker(AbstractWorker):
             print(url)
             print(data_uri)
 
-            ref = URIRef(url + '#char=' + str(current_char_count) + ',' + str(current_char_count + len(sentence)))
-
-            current_char_count += len(sentence) + 2
-
-            print(ref)
+            ref = URIRef(url + '#char=' + str(0) + ',' + str(len(sentence))) # TODO: fix offsets (when supported by server)
 
             nif.add((ref, RDF.type, namespaces.nif.RFC5147String))
             nif.add((ref, RDF.type, namespaces.nif.String))
             nif.add((ref, RDF.type, namespaces.nif.Context))
 
             nif.add((ref, namespaces.nif.isString, Literal(sentence, datatype=XSD.String)))
-
             nif_text = nif.serialize(format='turtle')
 
-            print('lorem')
             response = requests.post(kea_url, data=nif_text)
+            response_graph = Graph()
+            response_graph.parse(format='turtle', data=response.content)
 
-            print(response.content.decode('utf-8'))
-            print('ipsum')
-            
+            for subject in set(response_graph.subjects()):
+                # exceptions are ignored, because reformat_response_graph will fail when the given subject isn't a keyword that is recognized by kea, so that it won't be returned in our results
+                try:
+                    annotation = self.__class__.reformat_response_graph(response_graph, subject, current_char_count)
+                    annotations += annotation
+                except:
+                    pass
+                                                       
+            current_char_count += len(sentence) + 2
+            if current_char_count > max_length:
+                break
 
         self.write_and_merge_model(annotations, annotations_filename)
+
+    def reformat_response_graph(graph, subject, offset):
+        """
+        Extracts a subgraph for a single found keyword (if existing), adds an offset to the character positions and returns that graph
+        (as a temporary woraround while the kea service doesn't handle offsets correctly)
+        """
+
+        # generate new subject considering offset
+        match = re.search(r'^(?P<baseurl>.*)#char=(?P<begin>\d+),(?P<end>\d+)$', subject)
+        new_subject = URIRef(match.group('baseurl') + '#char=' + str(int(match.group('begin')) + offset) + ',' + str(int(match.group('end')) + offset))
+
+        model = Graph()
+        annotationNode = BNode()
+
+        model.add((annotationNode, RDF.type, namespaces.oa.Annotation))
+        model.add((annotationNode, namespaces.oa.hasTarget, new_subject))
+        model.add((annotationNode, namespaces.oa.hasBody, graph.value(subject, predicate=namespaces.itsrdf.taIdentRef)))
+        model.add((annotationNode, namespaces.oa.annotatedBy, graph.value(subject, predicate=DC.creator)))
+        
+        model.add((annotationNode, RDF.type, graph.value(subject, predicate=RDF.type)))
+        model.add((annotationNode, namespaces.nif.anchorOf, graph.value(subject, predicate=namespaces.nif.anchorOf)))
+        model.add((annotationNode, namespaces.nif.beginIndex, graph.value(subject, predicate=namespaces.nif.beginIndex) + offset))
+        model.add((annotationNode, namespaces.nif.endIndex, graph.value(subject, predicate=namespaces.nif.endIndex) + offset))
+        model.add((annotationNode, namespaces.itsrdf.taConfidence, graph.value(subject, predicate=namespaces.itsrdf.taConfidence)))
+
+        return model
 
 
 if __name__ == '__main__':
