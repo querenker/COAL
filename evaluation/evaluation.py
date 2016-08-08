ee744c09fca21aa80340642f7fc768c0d87d220e
Alexander Riese <alexander.riese@student.hpi.de>
2016-08-08 19:25:23 +0200

    init evaluation script

---
+++ evaluation/evaluation.py
@@ -0,0 +1,48 @@
+import subprocess
+import os
+import glob
+import tarfile
+import shutil
+
+test_data_directory = 'test-data/'
+
+
+def extractTarGzFile(filename):
+    try:
+        tarfile.open(filename, 'r:gz').extractall(test_data_directory).close()
+    except:
+        return
+
+
+def remove_unnecessary_files(directory):
+    unnecessary_file_type_extensions = 'jpg gif cif hkl wav tif doc'.split()
+    for extension in unnecessary_file_type_extensions:
+        for file in glob.iglob(directory + '/*/*.' + extension):
+            print(file)
+            os.remove(file)
+
+
+def remove_incomplete_data(test_data_directory):
+    for directory in glob.glob(test_data_directory + '/*'):
+        if len(glob.glob(directory + '/*.pdf')) != 1 or len(glob.glob(directory + '/*.nxml')) != 1:
+            shutil.rmtree(directory)
+
+
+def get_test_data(ftp_root_directory):
+    root_directory = ftp_root_directory
+    if ftp_root_directory.startswith('ftp://'):
+        root_directory = ftp_root_directory[len('ftp://'):]
+    root_directory = root_directory[:root_directory.find('/')]
+
+    if not os.path.exists(root_directory):
+        subprocess.run(['wget', '-r', ftp_root_directory])
+
+    for file in glob.iglob('ftp*/**/*.tar.gz', recursive=True):
+        extractTarGzFile(file)
+
+    remove_unnecessary_files(test_data_directory)
+    remove_incomplete_data(test_data_directory)
+
+
+if __name__ == '__main__':
+    get_test_data('ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/42/42/')
