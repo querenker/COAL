42f7a2fd880e4811b53b3e25a0a0c4d7029dc727
Alexander Riese <alexander.riese@student.hpi.de>
2016-08-08 20:17:49 +0200

    evaluation script extracts authors from xml and stores them in a txt file

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -3,6 +3,8 @@ import os
 import glob
 import tarfile
 import shutil
+from xml.etree import ElementTree
+from pathlib import Path
 
 test_data_directory = 'test-data/'
 
@@ -18,7 +20,6 @@ def remove_unnecessary_files(directory):
     unnecessary_file_type_extensions = 'jpg gif cif hkl wav tif doc'.split()
     for extension in unnecessary_file_type_extensions:
         for file in glob.iglob(directory + '/*/*.' + extension):
-            print(file)
             os.remove(file)
 
 
@@ -26,6 +27,16 @@ def remove_incomplete_data(test_data_directory):
     for directory in glob.glob(test_data_directory + '/*'):
         if len(glob.glob(directory + '/*.pdf')) != 1 or len(glob.glob(directory + '/*.nxml')) != 1:
             shutil.rmtree(directory)
+
+
+def get_authors_from_xml(xml_document):
+    file_directory = Path(xml_document).parent
+    xmldoc = ElementTree.parse(xml_document).getroot()
+    with open(str(file_directory) + '/authors.txt', 'w') as f: 
+        for author in xmldoc.findall('.//contrib'):
+            author_given_names = author.find('name/given-names').text
+            author_surname = author.find('name/surname').text
+            f.write(author_given_names + ' ' + author_surname + '\n')
 
 
 def get_test_data(ftp_root_directory):
@@ -46,3 +57,5 @@ def get_test_data(ftp_root_directory):
 
 if __name__ == '__main__':
     get_test_data('ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/42/42/')
+    for xml_file in glob.glob(test_data_directory + '/*/*.nxml'):
+        get_authors_from_xml(xml_file)
