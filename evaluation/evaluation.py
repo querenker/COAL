a6d4d0e3ee95abedf2d72a56483e5594841e3fd2
Alexander Riese <alexander.riese@student.hpi.de>
2016-08-09 19:48:01 +0200

    cermine algorithm benchmarking added

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -1,61 +1,64 @@
-import subprocess
 import os
-import glob
-import tarfile
-import shutil
+import requests
 from xml.etree import ElementTree
-from pathlib import Path
-
-test_data_directory = 'test-data/'
 
 
-def extractTarGzFile(filename):
-    try:
-        tarfile.open(filename, 'r:gz').extractall(test_data_directory).close()
-    except:
-        return
+test_data_directory = 'test-data'
 
 
-def remove_unnecessary_files(directory):
-    unnecessary_file_type_extensions = 'jpg gif cif hkl wav tif doc'.split()
-    for extension in unnecessary_file_type_extensions:
-        for file in glob.iglob(directory + '/*/*.' + extension):
-            os.remove(file)
+def compare_results(true_values, test_values):
+    assert len(true_values) == len(test_values)
+    true_positive_count = 0
+    false_positive_count = 0
+    false_negative_count = 0
+    for document in true_values:
+        true_authors = true_values[document]
+        find_authors = test_values[document]
+        true_positive_count += len(true_authors & find_authors)
+        false_positive_count += len(find_authors - true_authors)
+        false_negative_count += len(true_authors - find_authors)
+    print("true positive: " + str(true_positive_count))
+    print("false positive: " + str(false_positive_count))
+    print("false negative: " + str(false_negative_count))
 
 
-def remove_incomplete_data(test_data_directory):
-    for directory in glob.glob(test_data_directory + '/*'):
-        if len(glob.glob(directory + '/*.pdf')) != 1 or len(glob.glob(directory + '/*.nxml')) != 1:
-            shutil.rmtree(directory)
+def get_authors_from_file(authors_file):
+    return {line.strip() for line in open(authors_file, 'r')}
 
 
-def get_authors_from_xml(xml_document):
-    file_directory = Path(xml_document).parent
-    xmldoc = ElementTree.parse(xml_document).getroot()
-    with open(str(file_directory) + '/authors.txt', 'w') as f: 
-        for author in xmldoc.findall('.//contrib'):
-            author_given_names = author.find('name/given-names').text
-            author_surname = author.find('name/surname').text
-            f.write(author_given_names + ' ' + author_surname + '\n')
+def get_author_from_cermine_response(xml_response):
+    authors = set()
+    xmldoc = ElementTree.fromstring(xml_response)
+    for author in xmldoc.findall('.//contrib[@contrib-type="author"]'):
+        authors.add(author.find('string-name').text)
+    return authors
 
 
-def get_test_data(ftp_root_directory):
-    root_directory = ftp_root_directory
-    if ftp_root_directory.startswith('ftp://'):
-        root_directory = ftp_root_directory[len('ftp://'):]
-    root_directory = root_directory[:root_directory.find('/')]
+def get_author_mapping():
+    author_mapping = dict()
+    for path, _, files in os.walk(test_data_directory):
+        if path == test_data_directory:
+            continue
+        for file in files:
+            if file.endswith('.pdf'):
+                pdf_file = os.path.join(path, file)
+            if file.endswith('.txt'):
+                authors_file = os.path.join(path, file)
+        author_mapping[pdf_file] = get_authors_from_file(authors_file)
+    return author_mapping
 
-    if not os.path.exists(root_directory):
-        subprocess.run(['wget', '-r', ftp_root_directory])
 
-    for file in glob.iglob('ftp*/**/*.tar.gz', recursive=True):
-        extractTarGzFile(file)
-
-    remove_unnecessary_files(test_data_directory)
-    remove_incomplete_data(test_data_directory)
+def perform_cermine_algorithm(test_documents):
+    cermine_results = dict()
+    for document in test_documents:
+        print(document + ' in process...')
+        data = open(document, 'rb').read()
+        r = requests.post('http://cermine.ceon.pl/extract.do', data=data)
+        cermine_results[document] = get_author_from_cermine_response(r.text)
+    return cermine_results
 
 
 if __name__ == '__main__':
-    get_test_data('ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/42/42/')
-    for xml_file in glob.glob(test_data_directory + '/*/*.nxml'):
-        get_authors_from_xml(xml_file)
+    author_mapping = get_author_mapping()
+    cermine_results = perform_cermine_algorithm(author_mapping.keys())
+    compare_results(author_mapping, cermine_results)
