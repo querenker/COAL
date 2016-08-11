44415db92dd27a3f90a9007cdc5fc90c1a52e6c8
Alexander Riese <alexander.riese@student.hpi.de>
2016-08-11 15:35:21 +0200

    author finding improved

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -1,25 +1,33 @@
 import os
+import sys
 import requests
 from xml.etree import ElementTree
+import subprocess
 
+sys.path.insert(0, '../python-worker/')
+from author_candidate import AuthorCandidate
 
 test_data_directory = 'test-data'
 
 
 def compare_results(true_values, test_values):
     assert len(true_values) == len(test_values)
-    true_positive_count = 0
-    false_positive_count = 0
-    false_negative_count = 0
+    true_positives = set()
+    false_positives = set()
+    false_negatives = set()
     for document in true_values:
-        true_authors = true_values[document]
-        find_authors = test_values[document]
-        true_positive_count += len(true_authors & find_authors)
-        false_positive_count += len(find_authors - true_authors)
-        false_negative_count += len(true_authors - find_authors)
-    print("true positive: " + str(true_positive_count))
-    print("false positive: " + str(false_positive_count))
-    print("false negative: " + str(false_negative_count))
+        true_authors = set(true_values[document])
+        find_authors = set(test_values[document])
+        true_positives |= (true_authors & find_authors)
+        false_positives |= (find_authors - true_authors)
+        false_negatives |= (true_authors - find_authors)
+    print("true positive: " + str(len(true_positives)))
+    print("false positive: " + str(len(false_positives)))
+    print("false negative: " + str(len(false_negatives)))
+    print("-------------------false positive------------")
+    print(', '.join(false_positives))
+    print("-------------------false negative------------")
+    print(', '.join(false_negatives))
 
 
 def get_authors_from_file(authors_file):
@@ -34,6 +42,10 @@ def get_author_from_cermine_response(xml_response):
     return authors
 
 
+def extract_text(directory):
+    subprocess.run('for file in ./test-data/*/*.pdf; do pdftotext $file; done', shell=True)
+
+
 def get_author_mapping():
     author_mapping = dict()
     for path, _, files in os.walk(test_data_directory):
@@ -42,7 +54,7 @@ def get_author_mapping():
         for file in files:
             if file.endswith('.pdf'):
                 pdf_file = os.path.join(path, file)
-            if file.endswith('.txt'):
+            if file == 'authors.txt':
                 authors_file = os.path.join(path, file)
         author_mapping[pdf_file] = get_authors_from_file(authors_file)
     return author_mapping
@@ -58,7 +70,27 @@ def perform_cermine_algorithm(test_documents):
     return cermine_results
 
 
+def perform_our_algorithm(test_documents):
+    with open('../first-names/first_names.txt') as file:
+        first_names = {name.strip().casefold() for name in file.read().split('\n')}
+    with open('../words.txt') as file:
+        known_words = {word.strip().casefold() for word in file.read().split('\n')}
+    known_words -= first_names
+    stop_words = 'bureau univ school department institut ltd article science abstract research state'.split()
+
+    extract_text(test_data_directory)
+    our_results = dict()
+    for document in test_documents:
+        # print(document + ' in process...')
+        pdf_text = document[:-3] + 'txt'
+        author_text = open(pdf_text, 'rb').read().decode('utf-8')
+        authors = AuthorCandidate.get_authors_from_text(author_text, known_words, first_names, stop_words)
+        our_results[document] = authors
+    return our_results
+
+
 if __name__ == '__main__':
     author_mapping = get_author_mapping()
-    cermine_results = perform_cermine_algorithm(author_mapping.keys())
-    compare_results(author_mapping, cermine_results)
+    # cermine_results = perform_cermine_algorithm(author_mapping.keys())
+    our_results = perform_our_algorithm(author_mapping.keys())
+    compare_results(author_mapping, our_results)
--- python-worker/author_candidate.py
+++ python-worker/author_candidate.py
@@ -5,54 +5,60 @@ class AuthorCandidate:
     def __init__(self,
                  candidate_string,
                  stop_word_confidence = 0,
-                 author_list_confidence = 0,
                  known_name_confidence = 0,
                  known_title_confidence = 0,
                  name_pattern_confidence = 0,
                  name_word_count_confidence = 0,
-                 digit_word_count_confidence = 0):
+                 digit_word_count_confidence = 0,
+                 known_word_confidence = 0):
+
         self.candidate_string = candidate_string
-        
         self.stop_word_confidence = stop_word_confidence
-        self.author_list_confidence = author_list_confidence
         self.known_name_confidence = known_name_confidence
         self.known_title_confidence = known_title_confidence
         self.name_pattern_confidence = name_pattern_confidence
         self.name_word_count_confidence = name_word_count_confidence
         self.digit_word_count_confidence = digit_word_count_confidence
-        
+        self.known_word_confidence = known_word_confidence
+
     def score(self):
         return (self.stop_word_confidence * 10) \
-            + (self.author_list_confidence * 0) \
             + (self.known_name_confidence * 4) \
             + (self.known_title_confidence * 4) \
             + (self.name_pattern_confidence * 2) \
             + (self.name_word_count_confidence * .5) \
-            + (self.digit_word_count_confidence * 1)
-    
+            + (self.digit_word_count_confidence * 1) \
+            + (self.known_word_confidence * 1)
+
     def confidence(self):
         return 1 - (1 / sqrt(max(self.score(), 1)))
-    
-    def calculate_confidences(self, stop_words, first_names):
+
+    def calculate_confidences(self, stop_words, first_names, known_words):
         self.calculate_stop_word_confidence(stop_words)
-        self.calculate_author_list_confidence()
         self.calculate_author_pattern_confidence()
         self.calculate_known_name_confidence(first_names)
         self.calculate_known_title_confidence()
         self.calculate_name_word_count_confidence()
         self.calculate_digit_word_count_confidence()
-    
+        self.calculate_known_word_confidence(known_words)
+
+
+    def sanitized_candidate(self):
+        print(self)
+        author = self.candidate_string
+        author = ''.join(c for c in author if c.isalpha() or c in {' ', '.', '-', '’', chr(0x02ca), chr(0x00b4), chr(0x0301)}).strip()
+        author = re.sub(r'^\w | \w$', '', author)
+        if author.isupper():
+            author = author.title()
+        return author
+
     def calculate_stop_word_confidence(self, stop_words):
         'if one stop word is included, the specific confidence becomes 0, otherwise 1'
         self.stop_word_confidence = 0
         for stop_word in stop_words:
             if stop_word in self.candidate_string.casefold():
                 self.stop_word_confidence -= 1
-        
-    def calculate_author_list_confidence(self):
-        'calculates a confidence matching author lists by counting commas and "and"s'
-        self.author_list_confidence = sum([self.candidate_string.count(delimiter) for delimiter in [',', 'and']])
-        
+
     def calculate_author_pattern_confidence(self):
         'based on whether or not the words e.g. begin with an uppercase letter'
         self.name_pattern_confidence = 0
@@ -60,18 +66,19 @@ class AuthorCandidate:
             self.name_pattern_confidence = 1
         if sum(1 for c in self.candidate_string if c.isupper()) == 0:
             self.name_pattern_confidence = -1
-    
+
     def calculate_known_name_confidence(self, first_names):
         'based on whether or not the candidate includes an entry from a list of known names; name set has to be lowercase'
         # remove digits and split at commas and 'and's
         self.known_name_confidence = 0
-        for word in re.split(r' |,|and', ''.join([i for i in self.candidate_string if not i.isdigit()])):
+        words = re.split(r' |,|and', ''.join([i for i in self.candidate_string if not i.isdigit()]))
+        for word in words[:-1]:
             if word and word[0].isupper():
                 if len(word) == 2 and word[1] == '.':
                     self.known_name_confidence += 1
                 elif word.strip().casefold() in first_names:
                     self.known_name_confidence += 1
-                    
+
     def calculate_known_title_confidence(self):
         'based on whether or not the candidate includes an entry from a list of known titles, like Dr., or Prof.'
         # remove digits and split at commas and 'and's
@@ -80,13 +87,13 @@ class AuthorCandidate:
         for word in re.split(r' |,|and', ''.join([i for i in self.candidate_string if not i.isdigit()])):
             if word.casefold() in known_titles:
                 self.known_title_confidence += 1
-                    
+
     def calculate_name_word_count_confidence(self):
         '''based on the number of words the candidate consists of, a confidence is calculated.
         Normaly a name consists of two words (first and last name) but some more words are also common (multiple first names)'''
         word_count = sum(1 for word in self.candidate_string.split() if len(word) > 0 and word[0].isalpha())
         if word_count < 2:
-            self.name_word_count_confidence = -4
+            self.name_word_count_confidence = -16
         elif word_count == 2:
             self.name_word_count_confidence = 4
         elif word_count == 3:
@@ -97,10 +104,11 @@ class AuthorCandidate:
             self.name_word_count_confidence = 0
         else:
             self.name_word_count_confidence = -2
-            
+
     def calculate_digit_word_count_confidence(self):
         '''based on the number of digits in the words of the candidate a confidence is calculated.
         Normaly a name does not includ digits, but through the PDF to Text procedure footnote referenced are part of some candidates'''
+        self.digit_word_count_confidence = 0
         for word in self.candidate_string.split():
             digit_count = sum(1 for c in word if c.isdigit())
             if digit_count < 2:
@@ -111,14 +119,44 @@ class AuthorCandidate:
                 self.digit_word_count_confidence += -2
             else:
                 self.digit_word_count_confidence += -4
-            
+
+    def calculate_known_word_confidence(self, known_words):
+        self.known_word_confidence = 0
+        words = self.candidate_string.split() 
+        for word in words[:-1]:
+            if word.casefold() in known_words:
+                self.known_word_confidence -= 2
+        if words and words[-1].casefold() in known_words:
+            self.known_word_confidence -= 1
+
 
     def __repr__(self):
         return 'AuthorCandidate(' + str(round(self.confidence(), 3)) + ' \'' + self.candidate_string + '\' [' + \
                  str(self.stop_word_confidence) + ' ' + \
-                 str(self.author_list_confidence) + ' ' + \
                  str(self.known_name_confidence) + ' ' + \
                  str(self.known_title_confidence) + ' ' + \
                  str(self.name_pattern_confidence) + ' ' + \
                  str(self.name_word_count_confidence) + ' ' + \
+                 str(self.known_word_confidence) + ' ' + \
                  str(self.digit_word_count_confidence) + ']' + ')'
+
+    def preprocess_input(text):
+        '''
+        returns a list of AuthorCandidates, preprocessed by ignoring
+        blank lines and merging multi line entries
+        '''
+        author_candidates = []
+        for line in text.split('\n')[:20]:
+            line = re.sub(r'\s+', ' ', line)
+            for chunk in re.split(r',| and | AND | And | & ', line):
+                chunk = re.split(r'( |^)(by|BY|By) |: ', chunk)[-1]
+                author_candidates.append(AuthorCandidate(chunk.strip()))
+        return author_candidates
+
+    def get_authors_from_text(text, known_words, first_names, stop_words, min_confidence=0.5):
+        authors = set()
+        for candidate in AuthorCandidate.preprocess_input(text):
+            candidate.calculate_confidences(stop_words, first_names, known_words)
+            if candidate.confidence() >= min_confidence:
+                authors.add(candidate.sanitized_candidate())
+        return authors
